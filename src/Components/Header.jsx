b266aab6357e1bf1325ec53a2a70b4057c81c5e1
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-03-16 17:45:30 +0530

    initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+import { useState, useRef } from "react";
+import Button from "react-bootstrap/Button";
+import Overlay from "react-bootstrap/Overlay";
+import Popover from "react-bootstrap/Popover";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
+
+  const handleClick = (event) => {
+    setShow(!show);
+    setTarget(event.target);
+  };
+
+  return (
+    <div>
+      <div style={{ height: "100px", width: "100%", backgroundColor: "white" }}>
+        <img
+          className="ms-5"
+          height={"100px"}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAiIl9R-ixmJXHs4VMw3ixOxzYRZ5qThHwjg&s"
+          alt=""
+        />
+        <img
+          className="img-fluid "
+          style={{ position: "absolute", right: "100px", height: "100px" }}
+          src="https://cdn.archive.siasat.com/wp-content/uploads/2020/09/kerala-police.jpg"
+          alt=""
+        />
+      </div>
+      <header
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "16px 32px",
+          backgroundColor: "#796F57",
+
+          position: "sticky",
+          top: 0,
+          zIndex: 1000,
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "26px",
+            fontWeight: "bold",
+            color: "#F1F1F1",
+            letterSpacing: "1px",
+          }}
+        >
+          CopConnect
+          <i className="fa-solid fa-bars-staggered"></i>
+        </h1>
+        <button
+          className="btn btn-white text-white"
+          style={{
+            fontSize: "20px",
+            position: "absolute",
+            left: "300px",
+            backgroundColor: "#6D6249",
+          }}
+        >
+          <i class="fa-solid fa-bell"></i>
+        </button>
+
+        <button
+          className="btn-white rounded-3"
+          style={{ width: "300px", height: "45px" }}
+        >
+          <i className="fa-solid fa-fingerprint me-3"> </i> sign in with ID
+          Proof
+        </button>
+
+
+        <div ref={ref}>
+          <Button className="m-0 border-0" style={{ backgroundColor: "#6D6249" }} onClick={handleClick}>
+          <i class="fa-solid fa-language"></i>  Languages  <i class="fa-solid fa-caret-down"></i>
+          </Button>
+
+          <Overlay
+            show={show}
+            target={target}
+            placement="bottom"
+            container={ref}
+            containerPadding={20}
+          >
+            <Popover id="popover-contained">
+              <Popover.Header as="h3">Languages</Popover.Header>
+              <Popover.Body>
+                <strong>English</strong> Check this info.
+              </Popover.Body>
+              <Popover.Body>
+                <strong>Malayalam</strong> Check this info.
+              </Popover.Body>
+            </Popover>
+          </Overlay>
+        </div>
+
+        <nav>
+          <ul
+            style={{
+              display: "flex",
+              gap: "20px",
+              listStyle: "none",
+              margin: 0,
+              padding: 0,
+              fontSize: "18px",
+            }}
+          >
+            <li
+              style={{
+                cursor: "pointer",
+                color: "white",
+                transition: "color 0.3s",
+              }}
+            >
+              <button className="btn btn-white text-white m-0 ">
+                {" "}
+                Home <i class="fa-solid fa-house"></i>
+              </button>
+            </li>
+            <li
+              className="mt-1"
+              style={{
+                cursor: "pointer",
+                color: "#F1F1F1",
+                transition: "color 0.3s",
+              }}
+              //   onMouseOver={(e) => e.target.style.color = "#FF4C29"}
+              //   onMouseOut={(e) => e.target.style.color = "#F1F1F1"}
+            >
+              About
+            </li>
+            <li
+              className="mt-1"
+              style={{
+                cursor: "pointer",
+                color: "#F1F1F1",
+                transition: "color 0.3s",
+              }}
+              //   onMouseOver={(e) => e.target.style.color = "#FF4C29"}
+              //   onMouseOut={(e) => e.target.style.color = "#F1F1F1"}
+            >
+              Contact
+            </li>
+            <li
+              style={{
+                cursor: "pointer",
+                color: "#F1F1F1",
+                transition: "color 0.3s",
+              }}
+              //   onMouseOver={(e) => e.target.style.color = "#FF4C29"}
+              //   onMouseOut={(e) => e.target.style.color = "#F1F1F1"}
+            >
+              <button
+                style={{ backgroundColor: "#6D6249" }}
+                className="btn  text-white"
+              >
+                Login <i class="fa-solid fa-right-to-bracket"></i>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../Components/Header";
+
+const Home = () => {
+  return (
+    <div
+   
+    >
+      <Header />
+     
+    </div>
+  );
+};
+
+export default Home;
