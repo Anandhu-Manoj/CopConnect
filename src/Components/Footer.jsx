a662eebbecd130fb4c4e45a53e6b5250598d7670
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-03-17 17:24:23 +0530

    footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div
+        style={{
+          height: "100px",
+          width: "100%",
+          backgroundColor: "#796F57",
+          position: "fixed",
+          bottom: "0",
+          zIndex: "1",
+        }}
+      >
+        <img
+          style={{
+            width: "100%",
+            position: "absolute",
+            bottom: "10px",
+            zIndex: "-1",
+          }}
+          src="https://www.shjpolice.gov.ae/images/curves/lg-curve.png"
+          alt=""
+        />
+         <div
+        style={{
+          position: "absolute",
+          bottom: "100px",
+          right: "10px",
+          zIndex: "1",
+        }}
+      >
+        <img
+          className="img-fluid"
+          width={"60px"}
+          src="https://t4.ftcdn.net/jpg/07/29/34/35/360_F_729343514_Vd1VfwOsVmsvhMiR32V11yINk9Szo2Yq.jpg"
+          alt=""
+        />
+      </div>
+        <div className="row">
+          <div className="col-2 ms-5 text-white">
+            number of visitors <br />{" "}
+            <span style={{ fontWeight: "bold" }}>
+              38 million <i className="fa-solid fa-users"></i>{" "}
+            </span>
+          </div>
+
+          <div className="col-1 text-white">
+            Contact Us <i className="fa-solid fa-address-card"></i>
+          </div>
+          <div className="col-8">
+            <ul
+              className="d-flex  justify-center align-inline gap-1 text-white"
+              style={{ listStyle: "none" }}
+            >
+              <li>Disclaimer</li>|<li>Copyright</li>|<li>Privacy polict</li>|
+              <li>Site map</li>|
+              <li>Indian government promises for future service</li>|
+              <li>Accessibility</li>|<li>Terms and conditions</li>|
+            </ul>
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-center text-white">
+          All rights reserved @ 2025
+        </div>
+      </div>
+
+     
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
-    <div
-   
-    >
+    <div>
       <Header />
-     
+      <div className="ms-5 me-5 " ><marquee    behavior="" direction=""  >
+        Welcome to CopConnect - Your Digital Police Service Portal | File
+        Complaints | Book Appointments | Manage Criminal Data | Secure &
+        Transparent Policing{" "}
+      </marquee></div>
+      
+
+      <Footer />
     </div>
   );
 };
