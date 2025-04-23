aa4e78e7b9f6e655d0716f34f4f39e902e8e3214
Anandhu-Manoj <manojanandhu74@gmail.com>
2025-04-23 20:09:54 +0530

    preview and setting preview

--- src/pages/Local.jsx
+++ src/pages/Local.jsx
@@ -1,36 +1,84 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import Footer from '../Components/Footer'
-import Header from '../Components/Header'
-
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 
 const Local = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  const onLogin=()=>{
-    navigate('/services')
-
-  }
+  const onLogin = () => {
+    navigate("/services");
+  };
   return (
-    
-    <div className='overflow-hidden' style={{ margin: "0px", 
-      padding: '0px',boxSizing:"border-box"}}>
-        
-        <div className='d-flex justify-content-center align-items-center flex-column text-center gap-3 mt-3' style={{height:"70vh",marginTop:"-100px"}}>
-            <h2 ><i className="fa-solid fa-fingerprint fs-1 fw-bolder"></i></h2>
-            <h2  className='d-flex justify-content-center '>Sign in with id</h2>
-            <input style={{height:"70px"}} className='form-control w-25 shadow rounded-3' type="text" name="" id=""  placeholder='enter your email'/>
-            <input style={{height:"70px"}} className='form-control w-25 shadow rounded-3' type="text" name="" id=""  placeholder='enter your password'/>
-            <input style={{height:"70px"}} className='form-control w-25 shadow rounded-3' type="text" name="" id=""  placeholder='enter your aadhar number'/>
-            <button onClick={onLogin} className='w-25 rounded-3 border-0 shadow mt-2' style={{height:"50px",backgroundColor:"#ADB2D4"}}>login</button>
-           <Link to={'/'}> <button  className=' rounded-3 border-0 shadow mt-2' style={{height:"35px",backgroundColor:"#ADB2D4",width:"375px"}}>BACK</button></Link>
-            <div className='mt-3'> <p>dont have a INDIAN PASS  account? <Link to={'/localac'} className='text-success'>create new account</Link></p></div>
-
+    <div
+      className="overflow-hidden"
+      style={{ margin: "0px", padding: "0px", boxSizing: "border-box" }}
+    >
+      <div
+        className="d-flex justify-content-center align-items-center flex-column text-center gap-3 mt-3"
+        style={{ height: "70vh", marginTop: "-100px" }}
+      >
+        <h2>
+          <i className="fa-solid fa-fingerprint fs-1 fw-bolder"></i>
+        </h2>
+        <h2 className="d-flex justify-content-center ">Sign in with id</h2>
+        <input
+          style={{ height: "70px" }}
+          className="form-control w-25 shadow rounded-3"
+          type="text"
+          name=""
+          id=""
+          placeholder="enter your email"
+        />
+        <input
+          style={{ height: "70px" }}
+          className="form-control w-25 shadow rounded-3"
+          type="text"
+          name=""
+          id=""
+          placeholder="enter your password"
+        />
+        <input
+          style={{ height: "70px" }}
+          className="form-control w-25 shadow rounded-3"
+          type="text"
+          name=""
+          id=""
+          placeholder="enter your aadhar number"
+        />
+        <button
+          onClick={onLogin}
+          className="w-25 rounded-3 border-0 shadow mt-2"
+          style={{ height: "50px", backgroundColor: "#ADB2D4" }}
+        >
+          login
+        </button>
+        <Link to={"/"}>
+          {" "}
+          <button
+            className=" rounded-3 border-0 shadow mt-2"
+            style={{
+              height: "35px",
+              backgroundColor: "#ADB2D4",
+              width: "375px",
+            }}
+          >
+            BACK
+          </button>
+        </Link>
+        <div className="mt-3">
+          {" "}
+          <p>
+            dont have a INDIAN PASS account?{" "}
+            <Link to={"/localac"} className="text-success">
+              create new account
+            </Link>
+          </p>
         </div>
-        <Footer/>
-        </div>
-  )
-}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default Local
+export default Local;
--- src/pages/Localac.jsx
+++ src/pages/Localac.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../Components/Footer";
 
 const Localac = () => {
+  const [onReg, setOnReg] = useState({
+    adhaarImg: "",
+    username: "",
+    address: "",
+    password: "",
+    aadhar: "",
+  });
+
+  const [preview, setPreview] = useState("");
+  useEffect(() => {
+    if (
+      onReg.adhaarImg.type == "image/jpg" ||
+      onReg.adhaarImg.type == "image/png" ||
+      onReg.adhaarImg.type == "image/jpeg "
+    ) {
+      const View=URL.createObjectURL(onReg.adhaarImg)
+      setPreview(View);
+    } else {
+      console.log("please upload correct format");
+    }
+  }, [onReg.adhaarImg]);
+
+  const onSignUp = () => {
+    console.log(onReg);
+    console.log(preview)
+  };
+ 
+
   return (
-    <div style={{ margin: "0px",
-      padding: '0px',boxSizing:"border-box"}}>
+    <div style={{ margin: "0px", padding: "0px", boxSizing: "border-box" }}>
       <div
         className="d-flex justify-content-center align-items-center flex-column text-center gap-3 mt-3"
         style={{ height: "110vh", marginTop: "100px" }}
@@ -16,6 +43,9 @@ const Localac = () => {
         <h2 className="d-flex justify-content-center">Sign up</h2>
         <label>
           <input
+            onChange={(e) =>
+              setOnReg({ ...onReg, adhaarImg: e.target.files[0] })
+            }
             style={{ height: "70px", display: "none" }}
             className="form-control w-25 shadow rounded-3"
             type="file"
@@ -30,17 +60,18 @@ const Localac = () => {
               overflow: "hidden",
               marginLeft: "auto",
               marginRight: "auto",
-              marginBottom: "20px"
+              marginBottom: "20px",
             }}
           >
             <img
-              src="https://www.medianama.com/wp-content/uploads/2023/03/aadhaar-card-7579588_1280.png"
+              src={preview?preview:"https://www.medianama.com/wp-content/uploads/2023/03/aadhaar-card-7579588_1280.png"}
               className="img-fluid"
               alt=""
             />
           </div>
         </label>
         <input
+          onChange={(e) => setOnReg({ ...onReg, username: e.target.value })}
           style={{ height: "50px" }}
           className="form-control w-25 shadow rounded-3"
           type="text"
@@ -49,6 +80,7 @@ const Localac = () => {
           placeholder="Add user name"
         />
         <input
+          onChange={(e) => setOnReg({ ...onReg, address: e.target.value })}
           style={{ height: "50px" }}
           className="form-control w-25 shadow rounded-3"
           type="text"
@@ -57,6 +89,7 @@ const Localac = () => {
           placeholder="Enter your address details"
         />
         <input
+          onChange={(e) => setOnReg({ ...onReg, password: e.target.value })}
           style={{ height: "50px" }}
           className="form-control w-25 shadow rounded-3"
           type="password"
@@ -65,6 +98,7 @@ const Localac = () => {
           placeholder="Create your password"
         />
         <input
+          onChange={(e) => setOnReg({ ...onReg, aadhar: e.target.value })}
           style={{ height: "50px" }}
           className="form-control w-25 shadow rounded-3"
           type="text"
@@ -75,6 +109,7 @@ const Localac = () => {
         <button
           className="w-25 rounded-3 border-0 shadow mt-2"
           style={{ height: "50px", backgroundColor: "#ADB2D4" }}
+          onClick={onSignUp}
         >
           Sign Up
         </button>
